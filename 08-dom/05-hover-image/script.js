97dc7749747058453107beccebbb28b6dcdf266f
Julien Louis <julienlouis.becode@gmail.com>
2021-02-20 17:17:30 +0100

    progress

--- 08-dom/05-hover-image/script.js
+++ 08-dom/05-hover-image/script.js
@@ -11,6 +11,27 @@
 
 (function() {
 
+    var newImage = document.querySelector(".material figure img").getAttribute("data-hover");
+    var oldImage = document.querySelector(".material figure img").getAttribute("src");
+    console.log(newImage)
+
+    document.querySelector(".material figure img").removeAttribute("src")
+
+    document.querySelector(".material figure img").onmouseover = function() {mouseOver()};
+    document.querySelector(".material figure img").onmouseout = function() {mouseOut()};
+
+    function mouseOver() {
+        document.querySelector(".material figure img").src = newImage;
+    }
+    function mouseOut() {
+        document.querySelector(".material figure img").src = oldImage;
+    }
+
+
+    //document.querySelector(".material figure img").innerHTML = `onmouseover="${newImage}"`;
+
+
+
     // your code here
 
 })();
